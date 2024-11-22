33c1f399a0fc201482215ec98dc78deacbfb1252
trmk119 <trmk119@naver.com>
2024-11-22 15:04:53 +0900

    Add files via upload

---
+++ ch12_page41.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define SEED 17
+int MULT = 25173;
+int INC = 13849;
+int MOD = 65536;
+
+static unsigned int seed = SEED;// 난수 생성 시드값
+
+unsigned random_i(void)
+{
+	seed = (MULT * seed + INC) % MOD;
+	return seed;
+}
+
+double random_f(void)
+{
+	seed = (MULT * seed + INC) % MOD;
+	return seed / (double)MOD;
+}
